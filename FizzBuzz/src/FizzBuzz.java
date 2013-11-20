a98db3417b444b4de220a565d706e66613fe148c
mrmikemcguire <mcguire@mullenhigh.com>
2013-11-20 13:52:38 -0700

    Initial Commit
    This is a non-working version of FizzBuzz to practice debugging.

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class FizzBuzz
+	{
+	static ArrayList<String> listToPrint = new ArrayList();
+	
+	public static void main(String[] args)
+		{
+		for (int i=1;i<=15;i++)
+			{
+			listToPrint.add(String.valueOf(i));
+			System.out.println(listToPrint);
+
+			if ((i%3 == 0) && (i%5 != 0))
+				{
+				listToPrint.set(i,"fizz");
+				}
+			if ((i%5 == 0) && (i%3 != 0))
+				{
+				listToPrint.set(i,"buzz");
+				}
+			if ((i%3 == 0) && (i%5 == 0))
+				{
+				listToPrint.set(i,"fizzbuzz");
+				}
+			}
+			System.out.println(listToPrint);
+		}
+	}
+
+	
